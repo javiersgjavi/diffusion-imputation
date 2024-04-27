544521a9ff88f8a6d312df7adb0bbf59d90b1c94
Javier <javiersgjavi@gmail.com>
2024-04-27 17:05:40 +0200

    empeiza cambios añadir más datasets

--- src/utils_pristi.py
+++ src/utils_pristi.py
@@ -1,7 +1,63 @@
 import torch
-import os
+import pandas as pd
 import numpy as np
 import scipy.sparse as sp
+
+from sklearn.metrics.pairwise import haversine_distances
+
+
+def geographical_distance(x=None, to_rad=True):
+    _AVG_EARTH_RADIUS_KM = 6371.0088
+
+    # Extract values of X if it is a DataFrame, else assume it is 2-dim array of lat-lon pairs
+    latlon_pairs = x.values if isinstance(x, pd.DataFrame) else x
+
+    # If the input values are in degrees, convert them in radians
+    if to_rad:
+        latlon_pairs = np.vectorize(np.radians)(latlon_pairs)
+
+    distances = haversine_distances(latlon_pairs) * _AVG_EARTH_RADIUS_KM
+
+    # Cast response
+    if isinstance(x, pd.DataFrame):
+        res = pd.DataFrame(distances, x.index, x.index)
+    else:
+        res = distances
+
+    return res
+
+def get_similarity_AQI(dist, thr=0.1, include_self=False, force_symmetric=False, sparse=False):
+    theta = np.std(dist[:36, :36])  # use same theta for both air and air36
+    adj = thresholded_gaussian_kernel(dist, theta=theta, threshold=thr)
+    if not include_self:
+        adj[np.diag_indices_from(adj)] = 0.
+    if force_symmetric:
+        adj = np.maximum.reduce([adj, adj.T])
+    if sparse:
+        import scipy.sparse as sps
+        adj = sps.coo_matrix(adj)
+    return adj
+
+def get_similarity_pemsbay(thr=0.1, force_symmetric=False, sparse=False):
+    dist = np.load('./data/pems_bay/pems_bay_dist.npy')
+    finite_dist = dist.reshape(-1)
+    finite_dist = finite_dist[~np.isinf(finite_dist)]
+    sigma = finite_dist.std()
+    adj = np.exp(-np.square(dist / sigma))
+    adj[adj < thr] = 0.
+    if force_symmetric:
+        adj = np.maximum.reduce([adj, adj.T])
+    if sparse:
+        import scipy.sparse as sps
+        adj = sps.coo_matrix(adj)
+    return adj
+
+def get_adj_AQI36():
+    df = pd.read_csv("./data/pm25/SampleData/pm25_latlng.txt")
+    df = df[['latitude', 'longitude']]
+    res = geographical_distance(df, to_rad=False).values
+    adj = get_similarity_AQI(res)
+    return adj
 
 
 def get_similarity_metrla(thr=0.1, force_symmetric=False, sparse=False):
